db3fe1aeea2d1b7ef4cb2069a009ad4944d714d3
Sebastian <sebastian.paternostro@gmail.com>
2023-03-05 01:47:40 -0300

    Fixed home route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/cocktail-sp' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='cocktail/:id' element={<SingleCocktail />} />
         <Route path='*' element={<Error />} />
